dde6c3f3d0c630c3e8318242f599344761ccfeda
xrstf <git@xrstf.de>
2025-01-26 17:46:18 +0100

    graph basically done

--- pkg/pkigraph/graph.go
+++ pkg/pkigraph/graph.go
@@ -4,9 +4,13 @@
 package pkigraph
 
 import (
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"github.com/dominikbraun/graph"
 
 	"go.xrstf.de/pkiplot/pkg/types"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Graph struct {
@@ -20,23 +24,92 @@ func New() Graph {
 }
 
 func NewFromPKI(pki *types.PKI) Graph {
-	grph := New()
+	pg := New()
 
 	// add vertices for all PKI elements
 	for _, secret := range pki.Secrets {
-		grph.g.AddVertex(secretNode(secret))
+		pg.g.AddVertex(secretNode(secret))
 	}
 	for _, cert := range pki.Certificates {
-		grph.g.AddVertex(certificateNode(cert))
+		pg.g.AddVertex(certificateNode(cert))
 	}
 	for _, issuer := range pki.Issuers {
-		grph.g.AddVertex(issuerNode(issuer))
+		pg.g.AddVertex(issuerNode(issuer))
 	}
 	for _, clusterIssuer := range pki.ClusterIssuers {
-		grph.g.AddVertex(clusterIssuerNode(clusterIssuer))
+		pg.g.AddVertex(clusterIssuerNode(clusterIssuer))
 	}
 
-	return grph
+	for _, cert := range pki.Certificates {
+		hash := certificateHash(cert)
+
+		// create an edge between a cert and the secret it produces
+		if secretName := cert.Spec.SecretName; secretName != "" {
+			secretNode := pg.ensureSecret(cert.Namespace, secretName)
+			pg.g.AddEdge(secretNode.Hash(), hash)
+		}
+
+		// create an edge between a cert and its issuer
+		ref := cert.Spec.IssuerRef
+		switch ref.Kind {
+		case "", "Issuer":
+			issuerNode := pg.ensureIssuer(cert.Namespace, ref.Name)
+			pg.g.AddEdge(hash, issuerNode.Hash())
+		case "ClusterIssuer":
+			clusterIssuerNode := pg.ensureClusterIssuer(ref.Name)
+			pg.g.AddEdge(hash, clusterIssuerNode.Hash())
+		}
+	}
+
+	for _, issuer := range pki.Issuers {
+		hash := issuerHash(issuer)
+
+		// connect the secret that a CA issuer uses to sign new certs
+		if caConfig := issuer.Spec.CA; caConfig != nil && caConfig.SecretName != "" {
+			secretNode := pg.ensureSecret(issuer.Namespace, caConfig.SecretName)
+			pg.g.AddEdge(hash, secretNode.Hash())
+		}
+	}
+
+	return pg
+}
+
+func (g *Graph) ensureNode(n Node) Node {
+	n.Synthetic = true
+
+	vertex, err := g.g.Vertex(nodeHash(n))
+	if err != nil {
+		g.g.AddVertex(n)
+		return n
+	}
+
+	return vertex
+}
+
+func (g *Graph) ensureSecret(namespace, name string) Node {
+	return g.ensureNode(secretNode(corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      name,
+		},
+	}))
+}
+
+func (g *Graph) ensureIssuer(namespace, name string) Node {
+	return g.ensureNode(issuerNode(certmanagerv1.Issuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      name,
+		},
+	}))
+}
+
+func (g *Graph) ensureClusterIssuer(name string) Node {
+	return g.ensureNode(clusterIssuerNode(certmanagerv1.ClusterIssuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}))
 }
 
 func (g *Graph) Raw() graph.Graph[string, Node] {
--- pkg/pkigraph/node.go
+++ pkg/pkigraph/node.go
@@ -4,39 +4,94 @@
 package pkigraph
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
+
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Node struct {
-	Name string
+	Secret        *corev1.Secret
+	Certificate   *certmanagerv1.Certificate
+	Issuer        *certmanagerv1.Issuer
+	ClusterIssuer *certmanagerv1.ClusterIssuer
+
+	// Synthetic signal whether the object was actually found in the provided
+	// YAML manifests or if it was created based on reference names (e.g. a
+	// Certificate creating a Secret, but that Secret was not loaded in).
+	Synthetic bool
 }
 
-func nodeHash(c Node) string {
-	return c.Name
+func (n Node) Object() metav1.Object {
+	switch {
+	case n.Secret != nil:
+		return n.Secret
+	case n.Certificate != nil:
+		return n.Certificate
+	case n.Issuer != nil:
+		return n.Issuer
+	case n.ClusterIssuer != nil:
+		return n.ClusterIssuer
+	default:
+		panic("Invalid node: None of the four possible fields are set.")
+	}
+}
+
+func (n Node) Hash() string {
+	return objectHash(n.Object())
+}
+
+func nodeHash(n Node) string {
+	return objectHash(n.Object())
+}
+
+func objectHash(obj metav1.Object) string {
+	t := reflect.TypeOf(obj)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	kind := strings.ToLower(t.Name())
+
+	if ns := obj.GetNamespace(); ns != "" {
+		return fmt.Sprintf("%s:%s:%s", kind, ns, obj.GetName())
+	} else {
+		return fmt.Sprintf("%s:%s", kind, obj.GetName())
+	}
 }
 
 func secretNode(secret corev1.Secret) Node {
-	return Node{
-		Name: secret.Name,
-	}
+	return Node{Secret: &secret}
+}
+
+func secretHash(secret corev1.Secret) string {
+	return objectHash(&secret)
 }
 
 func certificateNode(cert certmanagerv1.Certificate) Node {
-	return Node{
-		Name: cert.Name,
-	}
+	return Node{Certificate: &cert}
+}
+
+func certificateHash(cert certmanagerv1.Certificate) string {
+	return objectHash(&cert)
 }
 
 func issuerNode(issuer certmanagerv1.Issuer) Node {
-	return Node{
-		Name: issuer.Name,
-	}
+	return Node{Issuer: &issuer}
+}
+
+func issuerHash(issuer certmanagerv1.Issuer) string {
+	return objectHash(&issuer)
 }
 
 func clusterIssuerNode(clusterIssuer certmanagerv1.ClusterIssuer) Node {
-	return Node{
-		Name: clusterIssuer.Name,
-	}
+	return Node{ClusterIssuer: &clusterIssuer}
+}
+
+func clusterIssuerHash(clusterIssuer certmanagerv1.ClusterIssuer) string {
+	return objectHash(&clusterIssuer)
 }
